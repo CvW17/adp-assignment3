e2eb60fc4d6ed7c6e2b0a50455db5bdfe58fb159
Tye <tyewalker09@gmail.com>
2021-06-08 17:39:48 +0200

    Created test class for Customer

--- src/main/java/za/ac/cput/Factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/Factory/CustomerFactory.java
@@ -20,7 +20,7 @@ public class CustomerFactory {
     //Method to validate email address:
     //Uses java.util.regex.Pattern.matches()
     static boolean validEmail(String custEmail) {
-        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        String regex = "^\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b";
         return custEmail.matches(regex);
     }
 
@@ -29,12 +29,13 @@ public class CustomerFactory {
         //UniqueID for customer
         String custID = UUID.randomUUID().toString();
 
-        if(custFirstName.isEmpty() || custLastName.isEmpty() || custContact.isEmpty() || custEmail.isEmpty() )
-            return null;
-
-        if(validEmail(custEmail)==false) {
+        //Customer cannot be created if email is invalid.
+        if(validEmail(custEmail) == false) {
             return null;
         }
+
+        if(custFirstName.isEmpty() || custLastName.isEmpty() || custContact.isEmpty() || custEmail.isEmpty() )
+            return null;
 
         return new Customer.Builder().custID("").custFirstName(custFirstName).custLastName(custLastName).custContact(custContact).custEmail(custEmail).builder();
     }
--- src/test/java/za/ac/cput/Entity/CustomerTest.java
+++ src/test/java/za/ac/cput/Entity/CustomerTest.java
@@ -7,6 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
 
+    String email = "tyewalker09@gmail.com";
+    String fakeEmail = "tye`walker09@gmail.com";
 
+    //Test for CustomerFactory.build
+    //This will fail if any fields are left bank and if the email is valid.
+    @Test
+    void validTest() {
+        Customer c = CustomerFactory.build("Tye","Walker","0827482442","218338562@mycput.ac.za");
+        System.out.println(c);
+        assertNotNull(c);
+    }
 
 }
