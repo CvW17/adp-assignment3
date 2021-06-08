cf52a7f928296df28e9d5f3148f1ce6920ac2956
RLX-Wilkinson <219071330@mycput.ac.za>
2021-06-08 23:42:29 +0200

    Added Code to AccountFactory

--- src/main/java/za/ac/cput/Factory/AccountFactory.java
+++ src/main/java/za/ac/cput/Factory/AccountFactory.java
@@ -1,5 +1,21 @@
 package za.ac.cput.Factory;
 
-public class AccountFactory {
+import za.ac.cput.Entity.Account;
 
+import java.util.*;
+
+public class AccountFactory {
+    public static Account createAccount(String accountBillingAddress, Date accountOpen, Date accountClose)
+    {
+       int ranNum = (int) Math.random();
+       Account acc = new Account.Builder()
+               .setCustomerID(ranNum)
+               .setAccountBillingAddress(accountBillingAddress)
+               .setAccountOpen(accountOpen)
+               .setAccountClose(accountClose)
+               .build();
+               return acc;
+
+
+    }
 }
