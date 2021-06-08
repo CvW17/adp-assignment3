0fe1e7f38ab8269f20a11c1a8bc3f76be9076ef1
RLX-Wilkinson <219071330@mycput.ac.za>
2021-06-08 13:30:35 +0200

    Created Test Files

---
+++ src/test/java/za/ac/cput/Entity/AccountTest.java
@@ -0,0 +1,15 @@
+package za.ac.cput.Entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+}
