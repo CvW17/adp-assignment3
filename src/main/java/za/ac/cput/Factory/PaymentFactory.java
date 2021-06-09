5c6945a5762700bc68d52e330b6af739ea7f4750
Cameron van Wyk <219088470@mycput.ac.za>
2021-06-10 00:40:22 +0200

    Small changes

--- src/main/java/za/ac/cput/Factory/PaymentFactory.java
+++ src/main/java/za/ac/cput/Factory/PaymentFactory.java
@@ -14,7 +14,7 @@ public class PaymentFactory
     public static Payment createPayment(String paymentDate, String paymentDetails, double paymentTotal)
     {
         String customerID = UUID.randomUUID().toString();
-        Payment payment = new Payment.Builder().setCusomterID(Integer.parseInt(customerID))
+        Payment payment = new Payment.Builder().setCusomterID(customerID)
                 .setPaymentDate(paymentDate)
                 .setPaymentDetails(paymentDetails)
                 .setPaymentTotal(paymentTotal).build();
