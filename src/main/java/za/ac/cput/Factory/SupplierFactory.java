a281397a5a96b3cc7c313efb55f421441cbae563
Robin White <robintylerw@gmail.com>
2021-06-09 23:35:10 +0200

    Factory pattern completed + All other tests added

--- src/main/java/za/ac/cput/Factory/SupplierFactory.java
+++ src/main/java/za/ac/cput/Factory/SupplierFactory.java
@@ -3,13 +3,30 @@ package za.ac.cput.Factory;
 /* SupplierFactory.java
 Factory for Supplier
 Author: Robyn White  (218034555)
-Date: 04 June 2021
+Date: 09 June 2021
  */
 
 
+import za.ac.cput.Entity.Supplier;
 
-
+import java.util.UUID;
 
 public class SupplierFactory {
 
+
+    public static Supplier build(String supplierName, String supplierEmail, String supplierPhone) {
+
+        String supplierID = UUID.randomUUID().toString();
+        String productID = UUID.randomUUID().toString();
+        if (supplierName.isEmpty() || supplierPhone.isEmpty() || supplierPhone.isEmpty())
+            return null;
+
+        return new Supplier.Builder()
+                .setSupplierID(supplierID)
+                .setProductID(productID)
+                .setSupplierName(supplierName)
+                .setSupplierEmail(supplierEmail)
+                .setSupplierPhone(supplierPhone)
+                .build();
+    }
 }
---
+++ src/test/java/za/ac/cput/Factory/SupplierFactoryTest.java
@@ -0,0 +1,58 @@
+package za.ac.cput.Factory;
+
+/* SupplierFactoryTest.java
+Factory Test for Supplier
+Author: Robyn White  (218034555)
+Date: 09 June 2021
+ */
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.Entity.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SupplierFactoryTest {
+
+    @Test                                                           //Tests to see if all fields have a value and is not null
+    void testCorrectData(){
+        Supplier s = SupplierFactory.build("CakeHouse","Cakeman@gmail.com","0216549132");
+        System.out.println(s);
+        assertNotNull(s);
+    }
+
+    @Test                                                           //Tests to see if all fields are null, returns null
+    void testIncorrectData(){
+        Supplier s = SupplierFactory.build("","","");
+        System.out.println(s);
+        assertNull(s);
+    }
+
+    @Test                                                           //Tests equality
+    void testEquality(){
+
+        Supplier s = SupplierFactory.build("CakeHouse","Cakeman@gmail.com","0216549132");
+        assertEquals(s,s);
+    }
+
+    @Test                                                           //Test Identity
+    void testIdentity(){
+        Supplier s = SupplierFactory.build("CakeHouse","Cakeman@gmail.com","0216549132");
+        assertSame(s,s);
+    }
+
+    @Test                                                           //Waits 5 seconds before test timesout
+    @Timeout(10)
+    void testTimeout() throws InterruptedException {
+            Thread.sleep(5000);
+            System.out.println("Test timeout");
+    }
+
+    @Disabled("Test has been Disabled")                             //Disables Test
+    @Test
+    void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
