b26f757c7fbfb2eab17be30e8482bd3d907bef0f
RLX-Wilkinson <81306123+RLX-Wilkinson@users.noreply.github.com>
2021-06-09 20:51:09 +0200

    Update AccountFactoryTest.java

--- src/test/java/za/ac/cput/Factory/AccountFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/AccountFactoryTest.java
@@ -8,11 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AccountFactoryTest {
 
-    @Test
-    public void createAccount(){
-        Account account = AccountFactory.createAccount("2415166161","26D Parkwood Avenue","26/02/2021",
-                "21/06/2021");
-        System.out.println(account);
-    }
+   
 
 }
