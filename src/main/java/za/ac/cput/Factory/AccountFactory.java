c285bea10e21e82b0d397854395252d9cb164382
RLX-Wilkinson <81306123+RLX-Wilkinson@users.noreply.github.com>
2021-06-09 20:50:11 +0200

    Entity Class Update

--- src/main/java/za/ac/cput/Factory/AccountFactory.java
+++ src/main/java/za/ac/cput/Factory/AccountFactory.java
@@ -5,17 +5,5 @@ import za.ac.cput.Entity.Account;
 import java.util.*;
 
 public class AccountFactory {
-    public static Account createAccount(String custID,String accountBillingAddress, String accountOpen, String accountClose)
-    {
-
-       Account acc = new Account.Builder()
-               .setCustID(custID)
-               .setAccountBillingAddress(accountBillingAddress)
-               .setAccountOpen(accountOpen)
-               .setAccountClose(accountClose)
-               .build();
-               return acc;
-
-
-    }
+   
 }
