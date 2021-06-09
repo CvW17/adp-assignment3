ad4b48e7c0f902b884485b5ebc8443cc37dba0c0
RLX-Wilkinson <219071330@mycput.ac.za>
2021-06-09 17:55:55 +0200

    Created a account

--- src/main/java/za/ac/cput/Factory/AccountFactory.java
+++ src/main/java/za/ac/cput/Factory/AccountFactory.java
@@ -1,5 +1,21 @@
 package za.ac.cput.Factory;
 
-public class AccountFactory {
+import za.ac.cput.Entity.Account;
 
+import java.util.*;
+
+public class AccountFactory {
+    public static Account createAccount(String custID,String accountBillingAddress, String accountOpen, String accountClose)
+    {
+
+       Account acc = new Account.Builder()
+               .setCustID(custID)
+               .setAccountBillingAddress(accountBillingAddress)
+               .setAccountOpen(accountOpen)
+               .setAccountClose(accountClose)
+               .build();
+               return acc;
+
+
+    }
 }
--- src/test/java/za/ac/cput/Factory/AccountFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/AccountFactoryTest.java
@@ -1,12 +1,18 @@
 package za.ac.cput.Factory;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.Entity.Account;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountFactoryTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void createAccount(){
+        Account account = AccountFactory.createAccount("2415166161","26D Parkwood Avenue","26/02/2021",
+                "21/06/2021");
+        System.out.println(account);
     }
+
 }
