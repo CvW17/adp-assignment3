030355bbd115812da061b74057383b85d1040f13
Tye <tyewalker09@gmail.com>
2021-06-10 15:03:43 +0200

    Added test cases to CustomerFactoryTest Object Identity Object Equality Timeouts Disabling Tests

--- src/test/java/za/ac/cput/Factory/CustomerFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/CustomerFactoryTest.java
@@ -10,13 +10,81 @@ package za.ac.cput.Factory;
 
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.Customer;
 import za.ac.cput.Factory.CustomerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerFactoryTest {
+
+    private CustomerFactory cust1;
+    private CustomerFactory cust2;
+    private CustomerFactory cust3;
+
+    @BeforeEach
+    void setUp() {
+        cust1 = new CustomerFactory();
+        cust2 = new CustomerFactory();
+        cust3 = cust1;
+    }
+
+    /*
+     * Object Equality:
+     * This test checks if the two objects are the same
+     */
+    @Test
+    void testEquality() {
+        assertEquals(cust1, cust3);
+    }
+
+    /*
+     * Object Identity:
+     * This test checks if the two objects are the same
+     * This test will pass:
+     */
+    @Test
+    void testIdentity() {
+        assertSame(cust1, cust3);
+    }
+    //This test will fail:
+    @Test
+    void testIdentityFail() {
+        assertSame(cust1, cust2);
+    }
+
+    /*
+     * Timeout Test:
+     * This test will fail after 5 seconds
+     */
+    //This test will pass:
+    @Test
+    @Timeout(5)
+    void testTimeout() throws InterruptedException {
+        Thread.sleep(4000);                         //Waits 5 seconds (5000 milliseconds)
+        System.out.println("Passed!");
+    }
+    //This test will fail:
+    @Test
+    @Timeout(5)
+    void testTimeoutFail() throws InterruptedException {
+        Thread.sleep(6000);                         //Waits 5 seconds (5000 milliseconds)
+        System.out.println("Passed!");
+    }
+
+
+    /*
+     * Disabling test:
+     * This test is disabled and will not run.
+     */
+    @Test
+    @Disabled
+    void DisabledTest() {
+        assertEquals(10, 5+2+6);
+    }
 
 
     //Test for CustomerFactory.build
