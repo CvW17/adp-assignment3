885790000b86b6add9f0d4eac1f2fa44f789604b
Cameron van Wyk <219088470@mycput.ac.za>
2021-07-23 11:16:02 +0200

    Added to Payment Repository

--- src/main/java/za/ac/cput/Repository/User/PaymentRepository.java
+++ src/main/java/za/ac/cput/Repository/User/PaymentRepository.java
@@ -7,15 +7,28 @@
 package za.ac.cput.Repository.User;
 
 import za.ac.cput.Entity.User.Payment;
+import za.ac.cput.Factory.PaymentFactory;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class PaymentRepository
 {
-    public Payment create(String customerID, String paymentDate, String paymentDetails, double paymentTotal)
+    private Set<Payment> paymentDescription;
+
+    public PaymentRepository()
     {
-        return null;
+        this.paymentDescription = new HashSet<>();
     }
 
-    public Payment read(Payment payment)
+    public Payment create(String customerID, String paymentDate, String paymentDetails, double paymentTotal)
+    {
+        Payment payment = PaymentFactory.createPayment(paymentDate, paymentDetails, paymentTotal);
+        this.paymentDescription.add(payment);
+        return payment;
+    }
+
+    public Payment read(String paymentID)
     {
         return null;
     }
