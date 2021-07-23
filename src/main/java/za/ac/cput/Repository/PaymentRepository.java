87c9c6cff93d3d002c4aa67f6a67f3d781ed2919
Cameron van Wyk <219088470@mycput.ac.za>
2021-07-23 10:12:05 +0200

    Create Payment Repository

---
+++ src/main/java/za/ac/cput/Repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+/* Payment.java
+ Entity for Payment Factory
+ Author: Cameron van Wyk (219088470)
+ Date: 23 July 2021
+*/
+
+package za.ac.cput.Repository;
+
+public class PaymentRepository
+{
+
+}
