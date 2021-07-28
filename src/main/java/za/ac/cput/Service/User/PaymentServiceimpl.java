397b918f2732eb710bcd3b6083b54ce4fcedf834
Cameron van Wyk <219088470@mycput.ac.za>
2021-07-28 11:21:21 +0200

    Code added to PaymentServiceimpl

--- src/main/java/za/ac/cput/Service/User/PaymentServiceimpl.java
+++ src/main/java/za/ac/cput/Service/User/PaymentServiceimpl.java
@@ -7,31 +7,53 @@
 package za.ac.cput.Service.User;
 
 import za.ac.cput.Entity.User.Payment;
+import za.ac.cput.Repository.User.PaymentRepository;
 
 public class PaymentServiceimpl implements PaymentService
 {
+    private static PaymentService service = null;
+    private PaymentRepository repository;
+
+    public PaymentServiceimpl(PaymentRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public PaymentServiceimpl()
+    {
+
+    }
+
+    public static PaymentService getService()
+    {
+        if (service == null)
+        {
+            service = new PaymentServiceimpl();
+        }
+        return service;
+    }
 
     @Override
     public Payment create(Payment payment)
     {
-        return null;
+        return this.repository.create(payment);
     }
 
     @Override
     public Payment read(String s)
     {
-        return null;
+        return this.repository.read(s);
     }
 
     @Override
     public Payment update(Payment payment)
     {
-        return null;
+        return this.repository.update(payment);
     }
 
     @Override
     public void delete(String s)
     {
-
+        this.repository.delete(s);
     }
 }
