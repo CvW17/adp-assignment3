414ce7af867a78b52e6b7ae23deb50021710a61f
Cameron van Wyk <219088470@mycput.ac.za>
2021-07-28 11:07:08 +0200

    Created Payment Service interface

---
+++ src/main/java/za/ac/cput/Service/User/PaymentService.java
@@ -0,0 +1,9 @@
+package za.ac.cput.Service.User;
+
+import za.ac.cput.Entity.User.Payment;
+import za.ac.cput.Service.IService;
+
+public interface PaymentService extends IService<Payment, String>
+{
+
+}
