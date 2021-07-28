281a209ab35b94c40eecbc3f68e10c559fb82590
Cameron van Wyk <219088470@mycput.ac.za>
2021-07-28 11:07:33 +0200

    Created Payment Service impl class

---
+++ src/main/java/za/ac/cput/Service/User/PaymentServiceimpl.java
@@ -0,0 +1,37 @@
+/* Payment.java
+ Entity for Payment Factory
+ Author: Cameron van Wyk (219088470)
+ Date: 28 July 2021
+*/
+
+package za.ac.cput.Service.User;
+
+import za.ac.cput.Entity.User.Payment;
+
+public class PaymentServiceimpl implements PaymentService
+{
+
+    @Override
+    public Payment create(Payment payment)
+    {
+        return null;
+    }
+
+    @Override
+    public Payment read(String s)
+    {
+        return null;
+    }
+
+    @Override
+    public Payment update(Payment payment)
+    {
+        return null;
+    }
+
+    @Override
+    public void delete(String s)
+    {
+
+    }
+}
