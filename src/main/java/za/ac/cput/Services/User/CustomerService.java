97ac115343f04e61c6696a9e1a2bde74a9d8a382
Tye <tyewalker09@gmail.com>
2021-08-02 17:23:10 +0200

    Fixed methods.

--- src/main/java/za/ac/cput/Services/User/CustomerService.java
+++ src/main/java/za/ac/cput/Services/User/CustomerService.java
@@ -1,9 +1,46 @@
 package za.ac.cput.Services.User;
 
 import za.ac.cput.Entity.User.Customer;
+import za.ac.cput.Repository.User.CustomerRepository;
 import za.ac.cput.Services.IService;
 
 
-public interface CustomerService extends IService<Customer, String> {
+public class CustomerService implements ICustomerService {
+    private static CustomerService customerService;
+    private CustomerRepository customerRepository;
 
+    private CustomerService() {
+        this.customerRepository = CustomerRepository.getRepository();
+    }
+
+    public static CustomerService getCustomerService() {
+        if (customerService == null) {
+            customerService = new CustomerService();
+        }
+        return customerService;
+    }
+
+    // Create:
+    @Override
+    public Customer create(Customer customer) {
+        return this.customerRepository.create(customer);
+    }
+
+    // Read:
+    @Override
+    public Customer read(String c) {
+        return this.customerRepository.read(c);
+    }
+
+    // Update:
+    @Override
+    public Customer update(Customer customer) {
+        return this.customerRepository.update(customer);
+    }
+
+    // Delete:
+    @Override
+    public void delete(String c) {
+        this.customerRepository.delete(c);
+    }
 }
---
+++ src/main/java/za/ac/cput/Services/User/ICustomerService.java
@@ -0,0 +1,9 @@
+package za.ac.cput.Services.User;
+
+import za.ac.cput.Entity.User.Customer;
+import za.ac.cput.Repository.User.CustomerRepository;
+import za.ac.cput.Services.IService;
+
+public interface ICustomerService extends IService<Customer, String> {
+
+}
