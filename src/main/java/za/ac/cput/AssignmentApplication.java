0e95b88a82ab5fb0182e77e7af269ea5db745f6b
Tye <tyewalker09@gmail.com>
2021-08-14 17:41:50 +0200

    Created main

---
+++ src/main/java/za/ac/cput/AssignmentApplication.java
@@ -0,0 +1,10 @@
+package za.ac.cput;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class AssignmentApplication {
+}
