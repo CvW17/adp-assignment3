cb781d49d576c2f9d1d59be2ec12735a444f43a8
Tye <tyewalker09@gmail.com>
2021-08-14 20:53:45 +0200

    Moved HomeController to correct file

--- src/main/java/za/ac/cput/controller/HomeController.java
+++ src/main/java/za/ac/cput/controller/HomeController.java
@@ -1,4 +1,4 @@
-package za.ac.cput.controller.user;
+package za.ac.cput.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
