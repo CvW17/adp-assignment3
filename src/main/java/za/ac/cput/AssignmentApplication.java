6a18fc03e0707c1e2a77afcdbee29fefbbf37b33
Tye <tyewalker09@gmail.com>
2021-08-14 17:48:53 +0200

    Updated main and created customer controller

--- src/main/java/za/ac/cput/AssignmentApplication.java
+++ src/main/java/za/ac/cput/AssignmentApplication.java
@@ -1,10 +1,20 @@
 package za.ac.cput;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class AssignmentApplication {
+    @RequestMapping({"/", "/home", "/index"})
+    public String home() {
+        return "Home Page";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
 }
